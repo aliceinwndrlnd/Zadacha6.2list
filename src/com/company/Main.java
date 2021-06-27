f2debcc0094226f64512f34a6d2b1f3aa1ea49d5
Alesya Yurkevich <lesja_1_gv@mail.ru>
2021-06-27 23:44:55 +0300

    chetnye oy 0 do 200

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 200; i++){
+            if (i % 2 == 0)
+        System.out.print(i + " ");}
+    }
+}
